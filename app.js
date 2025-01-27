a12e7c1f2aa648e4255e3dd136648460c4ffe14f
Arvellian <arvellianputra@gmail.com>
2025-01-28 05:06:38 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,16 @@
+// Import the express module
+const express = require('express');
+
+// Create an instance of express
+const app = express();
+
+// Define a simple route
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
+
+// Start the server on port 3000
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
